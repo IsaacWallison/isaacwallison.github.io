f81d48ed2344b666f12caf3e841cbd61e45a6dd7
Isaac Oliveira <Isaacsilva5@hotmail.com>
2024-08-31 14:32:39 -0300

    Add animation to sections

--- js/script.js
+++ js/script.js
@@ -2,23 +2,41 @@ const sidebarLinks = document.querySelectorAll('.sidebar-link');
 const sidebarToggle = document.querySelector('#sidebar-toggle');
 const starterSection = document.querySelector('#inicio');
 const navbar = document.querySelector('.navbar');
+const sections = document.querySelectorAll('section.container');
 
-const options = {
+const navbarObserver = new IntersectionObserver(changeNavbarTheme, {
+  root: null,
   rootMargin: '-200px',
-};
+});
 
-const observer = new IntersectionObserver(changeNavbarTheme, options);
+const sectionsObserver = new IntersectionObserver(animateSections, {
+  root: null,
+  threshold: 0.01,
+});
 
-observer.observe(starterSection);
+navbarObserver.observe(starterSection);
+sections.forEach((section) => {
+  sectionsObserver.observe(section);
+});
 
-function changeNavbarTheme(entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      navbar.classList.add('visible');
-    } else {
-      navbar.classList.remove('visible');
-    }
-  });
+function changeNavbarTheme(entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    navbar.classList.add('visible');
+    return;
+  }
+
+  navbar.classList.remove('visible');
+}
+
+function animateSections(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.add('animate');
+
+  observer.unobserve(entry.target);
 }
 
 sidebarLinks.forEach((link) => {
