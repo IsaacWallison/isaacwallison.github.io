af023a4a37454eecf208e236a03b80bccc19f558
Isaac Oliveira <Isaacsilva5@hotmail.com>
2024-10-08 21:37:07 -0300

    Add percentages animation of abilities section

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const sidebarToggle = document.querySelector('#sidebar-toggle');
 const starterSection = document.querySelector('#inicio');
 const navbar = document.querySelector('.navbar');
 const sections = document.querySelectorAll('section.container');
+const percentages = document.querySelector('.percentages');
 
 const navbarObserver = new IntersectionObserver(changeNavbarTheme, {
   root: null,
@@ -14,10 +15,16 @@ const sectionsObserver = new IntersectionObserver(animateSections, {
   threshold: 0.01,
 });
 
+const percentagesObserver = new IntersectionObserver(animatePercentages, {
+  root: null,
+  threshold: 0.1,
+});
+
 navbarObserver.observe(starterSection);
 sections.forEach((section) => {
   sectionsObserver.observe(section);
 });
+percentagesObserver.observe(percentages);
 
 function changeNavbarTheme(entries) {
   const [entry] = entries;
@@ -35,6 +42,27 @@ function animateSections(entries, observer) {
   if (!entry.isIntersecting) return;
 
   entry.target.classList.add('animate');
+
+  observer.unobserve(entry.target);
+}
+
+function animatePercentages(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  [
+    { abilitie: 'algorithms', percent: 55 },
+    { abilitie: 'html', percent: 90 },
+    { abilitie: 'css', percent: 85 },
+    { abilitie: 'js', percent: 80 },
+    { abilitie: 'react', percent: 65 },
+    { abilitie: 'node', percent: 60 },
+  ].forEach((values, i) => {
+    entry.target.querySelectorAll('.abilitie-percentage')[
+      i
+    ].style.width = `${values.percent}%`;
+  });
 
   observer.unobserve(entry.target);
 }
